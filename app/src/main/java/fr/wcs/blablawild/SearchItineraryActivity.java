c3a95ad7c53e343a60cf290a8d52fb8a317c294e
apprenti <bravo.pauline@gmail.com>
2017-02-27 14:41:11 +0100

    creation app blablawild

---
+++ app/src/main/java/fr/wcs/blablawild/SearchItineraryActivity.java
@@ -0,0 +1,13 @@
+package fr.wcs.blablawild;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchItineraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_itinerary);
+    }
+}

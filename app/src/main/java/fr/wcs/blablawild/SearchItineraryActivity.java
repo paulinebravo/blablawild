6e754e924d2be5279add71e2123f2ab6fad13392
apprenti <bravo.pauline@gmail.com>
2017-07-18 11:16:09 +0200

    add a date picker dialog

--- app/src/main/java/fr/wcs/blablawild/SearchItineraryActivity.java
+++ app/src/main/java/fr/wcs/blablawild/SearchItineraryActivity.java
@@ -1,15 +1,23 @@
 package fr.wcs.blablawild;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 import java.util.jar.Attributes;
 
-
+import static fr.wcs.blablawild.R.id.editTextDate;
 
 
 public class SearchItineraryActivity extends AppCompatActivity {
@@ -18,6 +26,8 @@ public class SearchItineraryActivity extends AppCompatActivity {
     EditText meditTextSearchDestination;
     EditText meditTextSearchDate;
     Button buttonSearch;
+    Calendar mCalendar;
+    DatePickerDialog.OnDateSetListener date;
 
     public final static String EXTRA_REQUEST = "Result";
     @Override
@@ -30,6 +40,27 @@ public class SearchItineraryActivity extends AppCompatActivity {
        meditTextSearchDate = (EditText) findViewById(R.id.editTextSearchDate);
        buttonSearch = (Button) findViewById(R.id.buttonSearch);
 
+        mCalendar = Calendar.getInstance();
+
+        date = new DatePickerDialog.OnDateSetListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                mCalendar.set(Calendar.YEAR, year);
+                mCalendar.set(Calendar.MONTH, month);
+                mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+
+            }
+        };
+
+        meditTextSearchDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(SearchItineraryActivity.this, date,
+                        mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
 
         buttonSearch .setOnClickListener(new View.OnClickListener()  {
@@ -70,6 +101,13 @@ public class SearchItineraryActivity extends AppCompatActivity {
         ;
 
 
+    }
+    private void updateLabel() {
+
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        meditTextSearchDate.setText(sdf.format(mCalendar.getTime()));
     }
 
 
